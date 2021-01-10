5f36ddf5849e551cac0e77645a59efe7582aa757
Claudio <claudio.goncalves@vistajet.com>
2021-01-10 18:13:23 -0300

    Update ECR repository path

--- packer/test_infra.py
+++ packer/test_infra.py
@@ -5,4 +5,4 @@ def test_configexiste(host):
     assert host.file("/root/.aws/credentials").exists    
 
 def test_dockerlogin(host):
-    assert host.run("aws ecr get-login-password | docker login --username AWS --password-stdin 848402301550.dkr.ecr.us-east-1.amazonaws.com/devops-getip")
+    assert host.run("aws ecr get-login-password | docker login --username AWS --password-stdin 933375035704.dkr.ecr.us-east-1.amazonaws.com/packer-terraform-ansible-deploy")
