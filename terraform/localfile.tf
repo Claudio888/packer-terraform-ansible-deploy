b939eacb55da5c2c6bbf6c9490970a9658f35358
Claudio <claudio.goncalves@vistajet.com>
2021-01-14 20:40:06 -0300

    add ansible_user=root

--- terraform/localfile.tf
+++ terraform/localfile.tf
@@ -1,7 +1,7 @@
 resource "local_file" "tf_host_ansible" {
   content = <<-DOC
     [Instance]
-    ${aws_eip.ip.public_ip}
+    ${aws_eip.ip.public_ip} ansible_user=root
     DOC
   filename = "../ansible/hosts"
 }
